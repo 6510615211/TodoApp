3af66c9690a38f37610cfb03b1cccfcfc1b4b8cd
ivy <6510615211@student.tu.ac.th>
2025-04-02 13:44:12 +0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by นางสาวพรนัชชา ประทีปสังคม on 2/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
